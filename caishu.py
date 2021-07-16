970a0a0f6151e5027ad9aa19788851ace3a2a870
yswang <977719395@qq.com>
2021-07-16 21:41:03 +0800

    add cishu

--- caishu.py
+++ caishu.py
@@ -1,8 +1,9 @@
 import random
 r=random.randint(1,100)
-
+i=0
 while(True):
 	num=int(input('你猜的数字：'))
+	i=i+1
 	if r<num:
 		print('比答案大')
 	elif r>num: 
@@ -11,3 +12,4 @@ while(True):
 		print('你猜对了')
 		break
 print('r=',r)
+print('你猜了',i,'次')
